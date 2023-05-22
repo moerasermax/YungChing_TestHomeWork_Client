29dabcd6e7381d6ba5e201c2c99214d608aacd7b
林郁宸 <moerasermax@gmail.com>
2023-05-22 19:38:30 +0800

    Clinet 端 補上Action_Flow

--- YungChing_TestHomeWork_Client/Model/RequestAction_CreateUser.cs
+++ YungChing_TestHomeWork_Client/Model/RequestAction_CreateUser.cs
@@ -15,6 +15,7 @@ namespace YungChing_TestHomeWork_Client.Model
         
         public override string Get_Reqeust_Command(DataSet_User_CRUD UserData)
         {
+            Action_Flow = Enum_Action.CREATEUSER;
             return string.Format("{0}..Split..{1}..Split..{2}", Enum_Action.CREATEUSER, "INSERT INTO [YungChing_TestHomework].[dbo].[User] VALUES(@Account,@Password,@Name,@Age);", JsonConvert.SerializeObject(UserData));
         }
     }
